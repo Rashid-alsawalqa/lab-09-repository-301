138335e4e3dc39a0e92cf4d34bbf89903c0bc8a9
asharaloran96 <asharoran96@gmail.com>
2020-01-04 14:44:50 +0200

    try to solve the null issue

--- modules/location.js
+++ modules/location.js
@@ -7,9 +7,11 @@ module.exports = getLocationData;
 
 const location = {};
 function Location(locationData) {
+    // this.search_query =locationData.city;
     this.formatted_query = locationData.display_name;
     this.latitude = locationData.lat;
     this.longitude = locationData.lon;
+    
 }
 
 
@@ -31,7 +33,7 @@ function getLocationData(city) {
 function cacheLocation(city, data) {
     const location = new Location(data[0]);
     let newSql = `INSERT INTO location(search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) RETURNING*`
-    let values = [city, location.display_query, location.latitude, location.longitude];
+    let values = [city, location.formatted_query, location.latitude, location.longitude];
     return client.query(newSql, values)
         .then(result => result.rows[0]);
 }
